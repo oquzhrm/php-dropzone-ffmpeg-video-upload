900d9149913f2bfefb5a45cb14719b4b0492f7cc
oquzhrm <oquz.hrm@hotmail.com>
2020-08-02 22:45:41 +0300

    php dropzone ffmpeg video upload

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DropZone.js & Video.js & FFMPEG & PHP Upload</title>
+    <title>DropZone.js & FFMPEG & PHP Upload</title>
     <link rel="stylesheet" href="./src/dropzone.css">
 </head>
 <body>
